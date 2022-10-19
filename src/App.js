e42b444014055861b2f732a0a742cf07e227490d
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-10-20 02:28:31 +0300

    add the component add user to the root component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import AddUser from './components/Users/AddUser';
 import './App.css';
 
 const App = () => {
   return (
-    <div></div>
+    <div>
+      <AddUser />
+    </div>
   )
 };
 
