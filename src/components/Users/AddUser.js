b4b580764f80b1840b5aabea28ed24e66c0c065d
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-10-20 02:27:31 +0300

    add a handler for adding a user

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -4,8 +4,13 @@ import React from 'react';
 // the property htmlFor helps readers to know which input, //
 // belongs to which label //
 const AddUser = props => {
+
+    const addUserHandler = (e) => {
+        e.preventDefault();
+    };
+
     return (
-        <form>
+        <form onSubmit={addUserHandler}>
             <label htmlFor='username'>Username</label>
             <input id='username' type='text' />
             <label htmlFor='age'>Age (Years)</label>
