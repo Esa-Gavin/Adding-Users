44b833e8f6817c840c2fcc1125bfe4d4eeb52f62
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-10-20 02:56:24 +0300

    add css module for add user component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 
+import classes from './AddUser.module.css';
+import Card from '../UI/Card';
 // the property htmlFor helps readers to know which input, //
 // belongs to which label //
 const AddUser = props => {
@@ -11,7 +13,7 @@ const AddUser = props => {
 
     return (
         <Card>
-            <form onSubmit={addUserHandler}>
+            <form className={classes.input} onSubmit={addUserHandler}>
                 <label htmlFor='username'>Username</label>
                 <input id='username' type='text' />
                 <label htmlFor='age'>Age (Years)</label>
