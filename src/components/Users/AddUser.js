3e369cbad9a01d32dfc362e809f4a6f4cf022add
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-10-20 02:22:50 +0300

    add a button for submitting

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -10,6 +10,7 @@ const AddUser = props => {
             <input id='username' type='text' />
             <label htmlFor='age'>Age (Years)</label>
             <input id='age' type='number' />
+            <button type='submit'>Add User</button>
         </form>
     )
 };
