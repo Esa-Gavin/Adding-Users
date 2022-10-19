719b3339d44db4c327aa3873ab7e2f7a4dc42d74
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-10-20 02:21:37 +0300

    add another label and input for age

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -8,6 +8,8 @@ const AddUser = props => {
         <form>
             <label htmlFor='username'>Username</label>
             <input id='username' type='text' />
+            <label htmlFor='age'>Age (Years)</label>
+            <input id='age' type='number' />
         </form>
     )
 };
