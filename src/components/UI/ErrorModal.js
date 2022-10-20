45ddedf89753e4accab04a9d76ff7870dc56fbdf
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-10-20 22:44:12 +0300

    add content to the error modal js file

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import classes from './ErrorModal.module.css';
+import Card from './Card';
+
+const ErrorModal = props => {
+    return (
+        <Card>
+            <header>
+                <h2></h2>
+            </header>
+            <div>
+                <p></p>
+            </div>
+            <footer></footer>
+        </Card>
+
+    );
+};
+
+export default ErrorModal;
