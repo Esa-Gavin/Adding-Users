485083aa32904d301becb86441656edafcddbf5c
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-10-20 03:14:57 +0300

    add comments

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import classes from './Card.module.css';
 
+// props.className can be named anything else, //
+// so long as you reflect the same name on, //
+// AddUser.js file //
 const Card = props => {
     return <div className={`${classes.card} ${props.className}`}>{props.children}</div>
 };
