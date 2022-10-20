46a2e965156c28dc7bc065f99bd124be919ddf39
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-10-20 17:10:39 +0300

    add props type in button component

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 import classes from './Button.module.css';
 
+// if a type is given for a button it shall be used, //
+// otherwise the default type is button //
+// || stands for OR //
 const Button = props => {
-    return <button className={classes.button}>Add User</button>
+    return <button className={classes.button} type={props.type || 'button'}>Add User</button>
 };
 
 export default Button;
