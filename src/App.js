a9a6d64689b9f1bb425147aef5d7a31c53f65e39
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-10-20 20:31:07 +0300

    create a call back function in the parent component so as to update the current state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import AddUser from './components/Users/AddUser';
 import './App.css';
+import UsersList from './components/Users/UsersList';
 
 const App = () => {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList(prevUsersList => {
+      return [...prevUsersList, {name: uName, age: uAge}]
+    })
+  }
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={[usersList]} />
     </div>
   )
 };
