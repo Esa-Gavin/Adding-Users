8a85567e8174021bd9a7071ef6dd68c1e49c3040
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-10-25 18:12:05 +0300

    learn how to use portals

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,25 +1,44 @@
 import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
 
 import classes from './ErrorModal.module.css';
 import Card from './Card';
 import Button from './Button';
 
+const Backdrop = props => {
+    return <div className={classes.backdrop} onClick={props.onConfirm} />
+}
+
+const ModalOverlay = props => {
+    return (
+        <Card className={classes.modal}>
+            <header className={classes.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={classes.content}>
+                <p>{props.message}</p>
+            </div>
+            <footer className={classes.actions}>
+                <Button onClick={props.onConfirm}>Okay</Button>
+            </footer>
+        </Card>
+    )
+}
+
+// 🙋 create portal method takes two arguments of, //
+// ReactNode and a pointer showing where the element, //
+// should be rendered in! //
+// 🤔 the onConfirm property represents the props.onConfirm in their //
+// respective functions //
 const ErrorModal = props => {
     return (
         <Fragment>
-            <div className={classes.backdrop} onClick={props.onConfirm} />
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onConfirm}>Okay</Button>
-                </footer>
-            </Card>
-
+            {ReactDOM.createPortal(
+                <Backdrop onConfirm={props.onConfirm} />,
+                document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(
+                <ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm} />,
+                document.getElementById('overlay-root'))}
         </Fragment>
 
 
