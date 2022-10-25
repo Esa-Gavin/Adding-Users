93b4c1b944bd80b2876efd0868b312e186b17806
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2022-10-25 19:34:45 +0300

    learn how to use refs in react

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useRef, useState } from 'react';
 
 
 import classes from './AddUser.module.css';
@@ -8,16 +8,25 @@ import ErrorModal from '../UI/ErrorModal';
 // the property htmlFor helps readers to know which input, //
 // belongs to which label //
 const AddUser = props => {
+    // 😇 so basically we have state and refs, for, //
+    // state it contains longer code but for, //
+    // refs the code is shorter but i prefer using refs instead, //
+    // so as to not complicate my code 😁 //
 
-    const [enteredUsername, setEnteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+
+    // const [enteredUsername, setEnteredUsername] = useState('');
+    // const [enteredAge, setEnteredAge] = useState('');
     const [error, setError] = useState();
 
     // the reason for line 18 and 19 is so as to, //
     // change the updated state back to an empty string //
     const addUserHandler = (e) => {
         e.preventDefault();
-        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+        const enteredJina = (nameInputRef.current.value);
+        const enteredMiaka = (ageInputRef.current.value);
+        if (enteredJina.trim().length === 0 || enteredMiaka.trim().length === 0) {
             setError({
                 title: 'Invalid input',
                 message: 'Please enter a valid name and age (non-empty values).'
@@ -26,25 +35,29 @@ const AddUser = props => {
         }
         // 👇 the unary operator is to help us change the enteredAge to a number, //
         // if it isn't ready //
-        if (+enteredAge < 1) {
+        if (+enteredMiaka < 1) {
             setError({
                 title: 'Invalid age',
                 message: 'Please enter a valid age (> 0).'
             })
             return;
         }
-        props.onAddUser(enteredUsername, enteredAge);
-        setEnteredUsername('');
-        setEnteredAge('');
+        props.onAddUser(enteredJina, enteredMiaka);
+        // 👇 here we are resetting the input values back to an, //
+        // empty string, but it is not advisable to do this with refs //
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
+        //setEnteredUsername('');
+        //setEnteredAge('');
     };
 
-    const usernameChangeHandler = (e) => {
-        setEnteredUsername(e.target.value);
-    }
+    // const usernameChangeHandler = (e) => {
+    //     setEnteredUsername(e.target.value);
+    // }
 
-    const ageChangeHandler = (e) => {
-        setEnteredAge(e.target.value);
-    }
+    // const ageChangeHandler = (e) => {
+    //     setEnteredAge(e.target.value);
+    // }
 
     const errorHandler = () => {
         setError(null);
@@ -62,9 +75,21 @@ const AddUser = props => {
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor='username'>Username</label>
-                    <input id='username' type='text' value={enteredUsername} onChange={usernameChangeHandler} />
+                    <input
+                        id='username'
+                        type='text'
+                        // value={enteredUsername}
+                        // onChange={usernameChangeHandler}
+                        ref={nameInputRef}
+                    />
                     <label htmlFor='age'>Age (Years)</label>
-                    <input id='age' type='number' value={enteredAge} onChange={ageChangeHandler} />
+                    <input
+                        id='age'
+                        type='number'
+                        // value={enteredAge}
+                        // onChange={ageChangeHandler}
+                        ref={ageInputRef}
+                    />
                     <Button type='submit'>Add User</Button>
                 </form>
             </Card>
